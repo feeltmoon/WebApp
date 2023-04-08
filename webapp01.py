65f35d4597c88441a8b035aa3b8b669a82fca895
feeltmoon <96011203+feeltmoon@users.noreply.github.com>
2023-04-08 23:10:36 +0800

    Add files via upload

---
+++ webapp01.py
@@ -0,0 +1,38 @@
+import streamlit as st  # pip install streamlit
+import pandas as pd  # pip install pandas
+import base64  # Standard Python Module
+from io import StringIO, BytesIO  # Standard Python Module
+
+
+def generate_excel_download_link(df):
+    # Credit Excel: https://discuss.streamlit.io/t/how-to-add-a-download-excel-csv-function-to-a-button/4474/5
+    towrite = BytesIO()
+    df.to_excel(towrite, encoding="utf-8", index=False, header=True)  # write to BytesIO buffer
+    towrite.seek(0)  # reset pointer
+    b64 = base64.b64encode(towrite.read()).decode()
+    href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download.xlsx">Download Excel File</a>'
+    return st.markdown(href, unsafe_allow_html=True)
+
+
+st.set_page_config(page_title='Excel Plotter')
+st.title('Excel Plotter 📈')
+st.subheader('Feed me with your Excel file')
+
+uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
+if uploaded_file:
+    st.markdown('---')
+    df = pd.read_excel(uploaded_file, engine='openpyxl')
+    st.dataframe(df)
+    groupby_column = st.selectbox(
+        'What would you like to analyse?',
+        ('Ship Mode', 'Segment', 'Category', 'Sub-Category'),
+    )
+
+    # -- GROUP DATAFRAME
+    output_columns = ['Sales', 'Profit']
+    df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
+
+
+    # -- DOWNLOAD SECTION
+    st.subheader('Downloads:')
+    generate_excel_download_link(df_grouped)
