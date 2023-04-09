b7e6a14d4b444d2356957a4fad9b76b50b144261
feeltmoon <96011203+feeltmoon@users.noreply.github.com>
2023-04-09 22:48:44 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -16,6 +16,9 @@ st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout
 # ---- READ EXCEL ----
 df = pd.read_excel("supermarkt_sales.xlsx", sheet_name="Sales")
 
+st.dataframe(df)
+st.markdown("""---""")
+
 # ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
 city = st.sidebar.multiselect(
