a70c899c4e1ca0f415218fd28e7e91679cb7a811
feeltmoon <96011203+feeltmoon@users.noreply.github.com>
2023-04-09 21:32:54 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -60,17 +60,17 @@ st.markdown("##")
 
 # TOP KPI's
 total_sales = int(df_selection["Total"].sum())
-average_rating = round(df_selection["Rating"].astype(np.float).astype('Int32').mean(), 1)
-star_rating = ":star:" * int(round(average_rating, 0))
+#average_rating = round(df_selection["Rating"].astype(np.float).astype('Int32').mean(), 1)
+#star_rating = ":star:" * int(round(average_rating, 0))
 average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
 
 left_column, middle_column, right_column = st.columns(3)
 with left_column:
     st.subheader("Total Sales:")
     st.subheader(f"US $ {total_sales:,}")
-with middle_column:
-    st.subheader("Average Rating:")
-    st.subheader(f"{average_rating} {star_rating}")
+#with middle_column:
+    #st.subheader("Average Rating:")
+    #st.subheader(f"{average_rating} {star_rating}")
 with right_column:
     st.subheader("Average Sales Per Transaction:")
     st.subheader(f"US $ {average_sale_by_transaction}")
