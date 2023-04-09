694043aba75893c11ad0d516acfc5f48e8dc32b4
feeltmoon <96011203+feeltmoon@users.noreply.github.com>
2023-04-09 22:49:29 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -16,6 +16,7 @@ st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout
 # ---- READ EXCEL ----
 df = pd.read_excel("supermarkt_sales.xlsx", sheet_name="Sales")
 
+st.markdown("""---""")
 st.dataframe(df)
 st.markdown("""---""")
 
