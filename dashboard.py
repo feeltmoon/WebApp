4098f9af652816b27988fa4e36168d9e52f25fa6
feeltmoon <96011203+feeltmoon@users.noreply.github.com>
2023-04-09 22:31:12 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -15,7 +15,7 @@ st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout
 
 # ---- READ EXCEL ----
 df = pd.read_excel(
-    "supermarkt_sales.xlsx",
+    io="supermarkt_sales.xlsx",
     engine="openpyxl",
     sheet_name="Sales",
     skiprows=3,
