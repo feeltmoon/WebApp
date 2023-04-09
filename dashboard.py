a6813a81c595d90405cf4428b62d1b7ca44c089e
feeltmoon <96011203+feeltmoon@users.noreply.github.com>
2023-04-09 22:38:57 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -17,10 +17,7 @@ st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout
 df = pd.read_excel(
     io="supermarkt_sales.xlsx",
     engine="openpyxl",
-    sheet_name="Sales",
-    skiprows=3,
-    usecols="B:R",
-    nrows=1000)
+    sheet_name="Sales")
 
 # ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
